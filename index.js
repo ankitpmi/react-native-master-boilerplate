c08e08c6ce191f2fb0251bf6e8332aa1624f2a62
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-07-09 17:28:04 +0530

    feat(script): added script for copying template

--- index.js
+++ index.js
@@ -1,105 +1,44 @@
 #!/usr/bin/env node
-
+const inquirer = require('inquirer');
 const { execSync } = require("child_process");
+const fs = require('fs-extra');
+const path = require('path');
 
-const inquirer = require("inquirer");
-const { writeFile, readFile, rmSync } = require("fs");
+const templates = {
+  expo: path.resolve(__dirname, 'templates', 'ExpoTemplate'),
+  rncli: path.resolve(__dirname, 'templates', 'CliTemplate')
+};
 
-const appName = process.argv[2];
+async function createProject() {
+  const { projectType, projectName } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'projectType',
+      message: 'Choose the type of project to create:',
+      choices: ['Expo', 'React Native CLI'],
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'Enter the project name:',
+    },
+  ]);
 
-// Function to log your details in the terminal
-const bareReactNativeAppGenerator = (name) => {
+  const templatePath = projectType === 'Expo' ? templates.expo : templates.rncli;
+  const targetPath = path.resolve(process.cwd(), projectName);
+
   try {
-    const command = `npx react-native init ${name} --template @mindinventory/react-native-boilerplate`;
-    execSync(`${command}`, { stdio: "inherit" });
-  } catch (e) {
-    console.error("Failed to execute ${command}", e);
-    return false;
+    console.log('Copying project files...');
+    await fs.copy(templatePath, targetPath);
+
+    console.log('yarn dependencies...');
+    // await execa('yarn', ['yarn'], { cwd: targetPath });
+    execSync(`cd ${process.cwd()}/${projectName} && yarn`, { stdio: "pipe" });
+
+    console.log(`Project ${projectName} created successfully!`);
+  } catch (error) {
+    console.error('Error creating project:', error);
   }
-  return true;
-};
-
-const expoAppGenerator = (name) => {
-  const cloneExpoProject = `git clone https://github.com/Mindinventory/expo-boilerplate.git ${name}`;
-  execSync(`${cloneExpoProject}`, { stdio: "pipe" });
-  const packagePath = `${process.cwd()}/${name}/package.json`;
-  const appConfigPath = `${process.cwd()}/${name}/app.json`;
-  const filesArr = [
-    {
-      path: packagePath,
-      type: "package",
-    },
-    {
-      path: appConfigPath,
-      type: "config",
-    },
-  ];
-  changeProjectNameForExpo(filesArr);
-  finalizeExpoProject();
-};
-
-const changeProjectNameForExpo = (pathArr) => {
-  for (let index = 0; index < pathArr.length; index++) {
-    const element = pathArr[index];
-
-    readFile(element.path, (error, data) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-
-      const parsedData = JSON.parse(data);
-
-      // // updating name in shipping_address
-      // console.log("parsedData ::: ", parsedData);
-      if (element.type === "config") {
-        parsedData.expo.name = appName;
-      } else {
-        parsedData.name = appName;
-      }
-
-      writeFile(element.path, JSON.stringify(parsedData, null, 2), (err) => {
-        if (err) {
-          console.log("Failed to write updated data to file");
-          return;
-        }
-        console.log("Updated file successfully");
-      });
-    });
-  }
-};
-
-const finalizeExpoProject = () => {
-  console.log("Project is created");
-  // execSync(`cd ${process.cwd()}/${appName}`, { stdio: "pipe" });
-  // execSync(`rm -rf .git*`, { stdio: "pipe" });
-  rmSync(`${process.cwd()}/${appName}/.git`, { recursive: true, force: true });
-  rmSync(`${process.cwd()}/${appName}/.gitignore`, {
-    recursive: true,
-    force: true,
-  });
-  execSync(`cd ${process.cwd()}/${appName} && yarn`, { stdio: "pipe" });
-};
-
-function main() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "Options",
-        message: "Select an tool from below list",
-        choices: ["Expo", "Bare React Native"],
-      },
-    ])
-    .then((answers) => {
-      if (answers.Options === "Bare React Native") {
-        // process.exit();
-        bareReactNativeAppGenerator(appName);
-      } else {
-        expoAppGenerator(appName);
-      }
-    });
 }
-if (require.main === module) {
-  main();
-}
+
+createProject();
