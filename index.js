cb25a48c57651eed2ec1ea81aa46097fddfb0e60
Ankit Prajapati <ankit.prajapati@mindinventory.com>
2024-08-20 11:46:33 +0530

    feat(script): fixed issue tracked git changes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ import { replaceInFile } from "replace-in-file"
 import { fileURLToPath } from "url"
 import { exec } from "child_process"
 import util from "util"
-import kleur from 'kleur';
+import kleur from "kleur"
 
-const {green, red} = kleur
+const { green, red } = kleur
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -25,12 +25,12 @@ function loadingAnimation(
   chars = ["⠙", "⠘", "⠰", "⠴", "⠤", "⠦", "⠆", "⠃", "⠋", "⠉"],
   delay = 100
 ) {
-  let x = 0;
+  let x = 0
 
-  return setInterval(function() {
-      process.stdout.write("\r" + chars[x++] + " " + getText());
-      x = x % chars.length;
-  }, delay);
+  return setInterval(function () {
+    process.stdout.write("\r" + chars[x++] + " " + getText())
+    x = x % chars.length
+  }, delay)
 }
 
 async function main() {
@@ -49,24 +49,9 @@ async function main() {
   const appJsonPath = path.join(destPath, "app.json")
 
   if (await fs.pathExists(destPath)) {
-    // const { overwrite } = await inquirer.prompt([
-    //   {
-    //     type: "confirm",
-    //     name: "overwrite",
-    //     message: `The directory "${projectName}" already exists. Do you want to overwrite it?`,
-    //     default: false,
-    //   },
-    // ]);
-
-    // if (!overwrite) {
-    //   console.log("Operation aborted.");
-    //   return;
-    // }    
-    console.log(red().bold(`The directory "${projectName}" already exists`));
-    // await fs.remove(destPath);
+    console.log(red().bold(`The directory "${projectName}" already exists`))
     return
   }
-
 
   const { boilerplate } = await inquirer.prompt([
     {
@@ -74,37 +59,28 @@ async function main() {
       name: "boilerplate",
       message: "Select the boilerplate to use:",
       choices: ["expo", "bare react native"],
-
-    }
-    
+    },
   ])
 
-  // const {  packageId } = await inquirer.prompt([   
-  //   {
-  //     type: "input",
-  //     name: "packageId",
-  //     message: "Enter the package ID (e.g., com.myapp):",
-  //     validate: (input) =>
-  //       /^[a-zA-Z]+\.[a-zA-Z]+/.test(input) ? true : "Package ID is not valid",
-  //     default: `com.${projectName.toLowerCase()}`,
-  //   },
-  // ])
-  let packageId = null;
-  if (boilerplate === 'bare react native') {
-    const response = await inquirer.prompt([   
+  let packageId = null
+  if (boilerplate === "bare react native") {
+    const response = await inquirer.prompt([
       {
         type: "input",
         name: "packageId",
         message: "Enter the package ID (e.g., com.myapp):",
         validate: (input) =>
-          /^[a-zA-Z]+\.[a-zA-Z]+/.test(input) ? true : "Package ID is not valid",
+          /^[a-zA-Z]+\.[a-zA-Z]+/.test(input)
+            ? true
+            : "Package ID is not valid",
         default: `com.${projectName.toLowerCase()}`,
       },
-    ]);
-    packageId = response.packageId;
+    ])
+    packageId = response.packageId
   }
 
-  const selectedType = boilerplate === "bare react native" ? "rncli" : boilerplate;
+  const selectedType =
+    boilerplate === "bare react native" ? "rncli" : boilerplate
 
   const srcPath = path.resolve(
     __dirname,
@@ -116,9 +92,11 @@ async function main() {
     let loader
     if (boilerplate === "expo") {
       await fs.copy(srcPath, destPath)
-      loader =loadingAnimation(() => `Setting up environment...`);      
-      clearInterval(loader);
-      loader =loadingAnimation(() => `Initializing new React Native project...`);
+      loader = loadingAnimation(() => `Setting up environment...`)
+      clearInterval(loader)
+      loader = loadingAnimation(
+        () => `Initializing new React Native project...`
+      )
       if (await fs.pathExists(packageJsonPath)) {
         const packageJson = await fs.readJson(packageJsonPath)
         packageJson.name = projectName
@@ -129,23 +107,27 @@ async function main() {
         const appJson = await fs.readJson(appJsonPath)
         appJson.expo.name = projectName
         await fs.writeJson(appJsonPath, appJson, { spaces: 2 })
-        clearInterval(loader);
-        console.log('\n');
-        console.log(green().bold('React Native EXPO project created successfully.'));
+        clearInterval(loader)
+        console.log("\n")
+        console.log(
+          green().bold("React Native EXPO project created successfully.")
+        )
       }
     } else {
       if (projectName && packageId) {
         let loader
         const projectDir = path.resolve(process.cwd())
-        loader =loadingAnimation(() => `Setting up environment...`);
+        loader = loadingAnimation(() => `Setting up environment...`)
         process.chdir(srcPath)
-  
+
         try {
           await execAsync(`npm link`)
           process.chdir(projectDir)
           await execAsync(`npm link @mindinventory/react-native-boilerplate`)
-          clearInterval(loader);  
-          loader =loadingAnimation(() => `Initializing new React Native project...`);
+          clearInterval(loader)
+          loader = loadingAnimation(
+            () => `Initializing new React Native project...`
+          )
           await execAsync(
             `npx react-native init ${projectName} --template @mindinventory/react-native-boilerplate --package-name ${packageId}`
           )
@@ -153,37 +135,46 @@ async function main() {
           console.error("Error during project setup:", error)
           return
         }
-  
+
+        // Remove the .git directory to uninitialize Git
+        const gitDirPath = path.join(destPath, ".git")
+        if (await fs.pathExists(gitDirPath)) {
+          await fs.remove(gitDirPath)
+        }
+
         // Verify if the project was created successfully
         if (await fs.pathExists(path.join(destPath, "package.json"))) {
+          
           const packageJsonFileIsExist = await fs.pathExists(packageJsonPath)
           if (packageJsonFileIsExist) {
             const packageJson = await fs.readJson(packageJsonPath)
             packageJson.name = projectName
             await fs.writeJson(packageJsonPath, packageJson, { spaces: 2 })
           }
-          execAsync("git init", { stdio: "inherit" });
-          clearInterval(loader);
-          console.log('\n');
-          
-          console.log(green().bold('React Native CLI project created successfully.'));
-  
+          process.chdir(destPath)
+
+          await execAsync("git init", { stdio: "inherit" });
+          await execAsync("git add .");
+          await execAsync(`git commit -m 'Initial commit'`);
+
+          clearInterval(loader)
+          console.log("\n")
+
+          console.log(
+            green().bold("React Native CLI project created successfully.")
+          )
         } else {
+          console.log("\n")
           console.error("Failed to create React Native CLI project.")
           return
         }
-      }else {
+      } else {
         return
       }
-     
     }
-
   } catch (err) {
     console.error("Error setting up boilerplate:", err)
   }
 }
 
 main()
-
-
-
